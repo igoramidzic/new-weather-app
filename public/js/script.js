eed29be9fe9b96b93fb05f2f94384ebdb30ab291
Igor Amidzic <amidzicigor@yahoo.com>
2016-12-20 22:13:07 -0500

    add search location feature

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,13 @@
 /* ------------------------------ Variables --------------------------------- */
-var lat, long, city, country, Ftemp, Ctemp, unitTemp, summary, icon, animation;
+var lat, long, city, country, Ftemp, Ctemp, unitTemp, summary, icon, animation,
+userSearchLocation, currentLocation;
 var currentUnit = 'F';
-var locationAPI = `https://maps.googleapis.com/maps/api/geocode/json?latlng=`
-var weatherAPI = `https://api.darksky.net/forecast/458b757b57430aa13526445cfd375d84/`
+var locationAPI = 'https://maps.googleapis.com/maps/api/geocode/json?latlng='
+var cityLocationAPI = 'https://maps.googleapis.com/maps/api/geocode/json?address='
+var weatherAPI = 'https://api.darksky.net/forecast/458b757b57430aa13526445cfd375d84/'
+
+
+$(document).ready(function () {
 
 /* ------------------------ Change animation function ----------------------- */
 function determineAnimation () {
@@ -69,6 +74,24 @@ function fetchCityAndCountry () {
   })
 }
 
+/* ------------------------- Fetch coords of location ----------------------- */
+function fetchCoordsOfLocation () {
+  $.ajax({
+    url: cityLocationAPI + `${userSearchLocation}`,
+    success: function (data) {
+      if (data.status === 'ZERO_RESULTS') {
+        $('#locationTextBox').attr('placeholder', 'Not found');
+      } else {
+        lat = data.results[0].geometry.location.lat;
+        long = data.results[0].geometry.location.lng;
+        fetchCityAndCountry();
+        fetchWeatherForCity();
+        $('#updateBtn').val('Your location');
+      }
+    }
+  })
+}
+
 /* -------------------------- Fetch weather for city ------------------------ */
 function fetchWeatherForCity () {
   $.ajax({
@@ -88,14 +111,13 @@ function fetchWeatherForCity () {
       $('#summary').text(summary);
       $('.weather-info').css('opacity', '1');
       $('.divToHide').css('opacity', '1');
-      // $('.divToHide').addClass('hide');
     }
   });
 }
 
 /* --------------------------- Geolocation function ------------------------- */
 function geolocation () {
-  $('#update').attr('disabled', 'disabled');
+  $('#updateBtn').attr('disabled', 'disabled');
   navigator.geolocation.getCurrentPosition(function (position) {
     lat = position.coords.latitude;
     long = position.coords.longitude;
@@ -107,27 +129,50 @@ function geolocation () {
     fetchWeatherForCity();
 
     // Return functionality to update button
-    $('#update').removeAttr('disabled');
+    $('#updateBtn').removeAttr('disabled');
 
   // If user does not have geolocation enabled
   }, function () {
-    $('#update').css('background-color', 'red');
-    $('#update').val('Error');
+    $('#updateBtn').css('background-color', 'red');
+    $('#updateBtn').val('Error');
   });
 }
 // Run on document initialize
 geolocation();
 
-// Run on document ready
-$(document).ready(function () {
-  $('#update').click(function () {
+  // User wants to update the weather data
+  $('#updateBtn').click(function () {
     $('.weather-info').css('opacity', '0.1');
     $('.divToHide').css('opacity', '0.1');
     $('.default').css('opacity', '0.1');
+    $(this).val('Update')
     geolocation();
   });
 
+  // User chooses unit measurment
   $('#unit').click(function () {
     changeUnit();
   })
+
+  // User searches for a location
+  $('#searchBtn').click(function () {
+    if ($('#locationTextBox').val()) {
+      userSearchLocation = $('#locationTextBox').val();
+      $('#locationTextBox').val('');
+      $('#locationTextBox').attr('placeholder', userSearchLocation);
+      fetchCoordsOfLocation();
+    }
+  })
+
+  $("#locationTextBox").keyup(function(event){
+    if(event.keyCode == 13){
+      if ($('#locationTextBox').val()) {
+        userSearchLocation = $('#locationTextBox').val();
+        $('#locationTextBox').val('');
+        $('#locationTextBox').attr('placeholder', userSearchLocation);
+        fetchCoordsOfLocation();
+      }
+    }
+  });
+
 });
